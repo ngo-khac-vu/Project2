f444fafb7d47a1bb376ec6d4bda9ec9b1184837f
ngo-khac-vu <vu.ngokhac.93@gmail.com>
2021-05-28 19:12:38 +0700

    Init
    Init project

---
+++ Project2/DataAccess/Nhibernate/INhibernateSessionFactory.cs
@@ -0,0 +1,12 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+
+namespace DataAccess.Nhibernate
+{
+    public interface INhibernateSessionFactory
+    {
+        ISession CreateSession();
+
+        Configuration GetConfiguration(string connectionAlias);
+    }
+}
